c70168d635ecc4d7c6e31375b287539d318902d5
NicolasGDas <50321430+NicolasGDas@users.noreply.github.com>
2022-01-02 14:36:34 -0300

    Refactor name path

--- src/Compartido/lector_datos/obtener_datos.js
+++ src/Compartido/lector_datos/obtener_datos.js
@@ -2,7 +2,7 @@ import { promises as fs } from 'fs'
 
 async function obtener_datos(usuario_requerido){
 
-        let datos = JSON.parse(await fs.readFile(`../Entrega_3/src/Venta/Persistencia/Interno/Documentos/${usuario_requerido}.json`))
+        let datos = JSON.parse(await fs.readFile(`../Api MercadoPago/src/Venta/Persistencia/Interno/Documentos/${usuario_requerido}.json`))
         return datos
         
 }
