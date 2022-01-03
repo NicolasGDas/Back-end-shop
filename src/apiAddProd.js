1558026e0777bfad0f18f0c8af5ccbc886f87cd2
NicolasGDas <50321430+NicolasGDas@users.noreply.github.com>
2022-01-02 23:35:51 -0300

    Add new api for product

---
+++ src/apiAddProd.js
@@ -0,0 +1,18 @@
+import Factory_CU_add_product from "./Venta/Negocio/Caso_de_uso_add_product/Factory_CU_add_product.js";
+import Dao_ProductsFactory from "./Venta/Persistencia/Dao_ProductsFactory.js";
+
+
+function crearApiProd(){
+    const apiProd = {
+        addProd: async (product)=>{
+            const cu_add_prod = Factory_CU_add_product.crearCU_add_prod()
+            await cu_add_prod.realizar(product)
+        },
+        cerrar: async() =>{
+            const DaoProduct = Dao_ProductsFactory.getDaoProducts()
+            await DaoProduct.cerrar()
+        }
+    }
+    return apiProd
+}
+export{crearApiProd}
